642f23ff18c412f371cab5a4e9a78eda88e36c2a
Lindsey Maddox <llmaddox@gmail.com>
2017-10-15 07:09:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var app = express();
+
+ var path = require('path');
+app.use(express.static(path.resolve(__dirname + '/public')));
+
+   app.get('/', function(req,res){
+     res.sendFile(path.join(__dirname+'/sandbox.html'));
+});
+
+      app.get('/choropleth', function(req,res){
+     res.sendFile(path.join(__dirname+'/choropleth-pop.html'));
+});
+
+  app.listen(process.env.PORT || 3000, function () {
+     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);;
+  });
