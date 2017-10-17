cc61c6b73d5de73a5910a9d8b80a71ca54f2a70c
Lindsey Maddox <llmaddox@gmail.com>
2017-10-17 06:00:07 -0400

    style and add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+#D3 Maps
+
+A sandbox for working with d3 maps based on US census population data.
+
+##Choropleth map
--- index.js
+++ index.js
@@ -2,17 +2,17 @@
 var express = require('express');
 var app = express();
 
- var path = require('path');
+var path = require('path');
 app.use(express.static(path.resolve(__dirname + '/public')));
 
-   app.get('/', function(req,res){
-     res.sendFile(path.join(__dirname+'/index.html'));
+app.get('/', function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
 });
 
-      app.get('/choropleth', function(req,res){
-     res.sendFile(path.join(__dirname+'/choropleth-pop.html'));
+app.get('/choropleth', function(req,res){
+  res.sendFile(path.join(__dirname+'/choropleth-pop.html'));
 });
 
-  app.listen(process.env.PORT || 3000, function () {
-     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);;
-  });
+app.listen(process.env.PORT || 8080, function () {
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);;
+});
